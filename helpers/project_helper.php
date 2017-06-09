6222e476c1e2748eabaa086ddd95771b304c0653
Trigur <trigur@yandex.ru>
2017-06-09 15:08:27 +0300

    Update project_helper.php

--- helpers/project_helper.php
+++ helpers/project_helper.php
@@ -58,7 +58,7 @@ if (! function_exists('getAlbum')) {
 }
 
 if (! function_exists('getAlbums')) {
-    function getAlbums($categoryId = 0, $sortBy = 'position', $sortOrder = 'desc')
+    function getAlbums($categoryId = 0, $sortBy = 'date', $sortOrder = 'desc')
     {
         $ci = & get_instance();
         return $ci->load->module('gallery')->load->model('gallery_m')->get_albums($sortBy, $sortOrder, $categoryId);
